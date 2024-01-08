7060975935d3ff9ac2bd53c8eef10eb21bbf74e8
palakgoel30 <gpalak7055@gmail.com>
2024-01-08 13:29:50 +0530

    Data types

--- Chapter1.js
+++ Chapter1.js
@@ -1,7 +1,26 @@
-console.log("Hello JS!!!!");
+console.log("Hello JS!!!!"); // to print 
 
-name = "Palak"
+name = "Palak" // Variable
 console.log(name)
 x= null;
 y= undefined;
 console.log(x,y);
+
+isFollow = 'True'
+console.log(isFollow) 
+console.log(typeof isFollow)  // typeof used to check the type of a variable.
+
+// Object DataType
+
+const student = {
+    fullNane : 'Ram',
+    age : 23,
+    cgpa : 8.9,
+    isPass : 'True'
+};
+
+console.log(student)
+console.log(student['age'])   //23
+console.log(student.cgpa)     //8.9
+console.log(typeof student)   // Object
+console.log(typeof student["cgpa"]) // number
