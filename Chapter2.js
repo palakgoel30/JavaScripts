3fb66cfd45d8a2425266db851272cb3e9b9ef9c7
palakgoel30 <gpalak7055@gmail.com>
2024-01-09 21:55:55 +0530

    Conditional Statement

---
+++ Chapter2.js
@@ -0,0 +1,47 @@
+// single file comment
+
+/* multiple
+line
+comment */
+
+//Conditional Statement
+//If statement
+let age = 25;
+if(age > 18){
+    console.log("You can vote")
+}
+
+if(age< 18){
+    console.log("You cann't vote")
+}
+
+//If-else statement
+
+let Age = 18;
+if(Age > 18){
+    console.log("You can vote");
+}
+
+else
+{
+    console.log("You cann't vote");
+}
+
+//else-if statement
+
+let mode = 'Blue';
+let color;
+if(mode == 'dark'){
+    color = 'Black';
+}
+else if(mode == 'pink'){
+    color = 'pink';
+}
+else if(mode == 'Blue'){
+    color = 'blue';
+}
+else{
+    color = 'white';
+}
+
+console.log(color);
