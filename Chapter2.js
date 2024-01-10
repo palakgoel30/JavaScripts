dd23d8ca73620b3794e178998b15b9af7abcdad4
palakgoel30 <gpalak7055@gmail.com>
2024-01-10 13:57:25 +0530

    Prompt & Alert

--- Chapter2.js
+++ Chapter2.js
@@ -45,3 +45,54 @@ else{
 }
 
 console.log(color);
+
+// Ternary Operators
+
+let a = 56
+
+let result = a >=18? "Yes":"No";
+console.log(result)
+
+let A = 56
+
+A <=18? console.log("Yes"):console.log("No");
+
+// Practise Question 
+
+//Get user to input a number using prompt("Enter a number"). check if the number is multiple of 5 or not
+
+//Prompt
+
+let name = prompt("Enter your name");
+console.log(name);
+
+//Problem
+
+let number = prompt("Enter a number");
+
+if(number % 5 == 0){
+    console.log("Yes, Disible by 5")
+}
+else{
+    console.log("No, Not disible by 5")
+}
+
+// Write a code which can give grades to student according to their score
+
+let score = prompt("Enter a score of student to find the grade:");
+
+if(score >= 0 && score <= 45){
+    console.log("F")
+}
+else if(score >= 50 && score <= 59){
+    console.log("D")
+}
+else if(score >= 60 && score <= 69){
+    console.log("c")
+}
+else if(score >= 70 && score <= 79){
+    console.log("B")
+}
+else if(score >= 80 && score <= 100){
+    console.log("A")
+}
