9a619e6af0a388b63916109e794ec52278f18cc0
palakgoel30 <gpalak7055@gmail.com>
2024-01-16 12:56:33 +0530

    String Methods

--- Chapter3.js
+++ Chapter3.js
@@ -42,3 +42,37 @@ let Student = {
 for(let key in Student){
     console.log("key :",key, "Value: ",Student[key]);
 }
+
+// String
+let str1 = "ApnaNaam"
+console.log(str1.length) // 8
+console.log(str1[3]) //a
+
+//Template Literals
+
+let obj = {
+    item :'pen',
+    price : 10
+};
+
+console.log("The cost of ",obj.item," is ",obj.price," ruppes")  // The cost of pen is 10 ruppes.
+
+//using template literals
+
+let Object = `The cost of ${obj.item} is ${obj.price} ruppes`;
+console.log(Object)
+
+// String Methods
+
+let strings = 'Hello'
+console.log(strings.slice(0,3)) // 'Hel'
+
+let strings2 = 'pooja'
+console.log(strings.concat(strings2)) // 'Hellopooja'
+
+console.log(strings.replace('lo','p')) // 'Help'
+
+// Practice Question
+let fullName = prompt('Enter your full name');
+let username = `@${fullName}${fullName.length}`
+console.log(username)
