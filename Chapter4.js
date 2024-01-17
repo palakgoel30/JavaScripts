20e0b341c9ef652eec6ebfb5f0de79f0113a3ed2
palakgoel30 <gpalak7055@gmail.com>
2024-01-17 21:20:36 +0530

    Array

---
+++ Chapter4.js
@@ -0,0 +1,36 @@
+// Array
+
+let heros = ['Ironman','Spiderman','Amitab','Dharanjay','Thor']
+
+// for loop
+for(let i = 0;i < heros.length;i++){
+    console.log(heros[i]);
+}
+
+// for-of loop
+for(let hero of heros){
+    console.log(hero);
+}
+
+//Practice Question1
+
+let marks = [85,97,44,37,76,60]
+let sum = 0
+for(let mar = 0; mar <marks.length;mar++){
+    sum = sum+ marks[mar];
+}
+console.log(`The average marks of class ${sum/marks.length}`)
+
+//Practice Question2
+
+let prices = [250,645,300,900,50]
+for (let dis = 0; dis <5;dis++){
+    prices[dis] = prices[dis] - (prices[dis]*0.1)
+}
+console.log(prices)
+
+// Push method
+
+let footItems = ['Potato','Chips','Maggie','Green Tea'];
+footItems.push("Burger","Pizza");
+console.log(footItems);
